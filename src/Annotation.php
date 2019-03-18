05b85d5f7e4dfc1fc2ad549cc078aba9b8f12a92
Carlos Antônio <hpcarlos20@gmail.com>
2019-03-18 03:55:18 -0300

    Update Annotation.php

--- src/Annotation.php
+++ src/Annotation.php
@@ -1,17 +1,19 @@
 <?php
 
-namespace qsun\ModelAnnotation;
+namespace hpcarlos\model_annotation;
 
 use Log;
 use ReflectionClass;
 use DB;
+use Illuminate\Support\Pluralizer;
 
 class Annotation {
     public static function annotateTable($app, $table) {
         foreach (glob("{$app->path}/*.php") + glob("{$app->path}/**/*.php") as $file) {
             $filename = basename($file);
             $name = substr($filename, 0, strlen($filename)-4);
-            $possible_class_name = studly_case(str_singular($table));
+            $singular_name = Pluralizer::singular($table);
+            $possible_class_name = studly_case($singular_name);
 
             if ($name == $possible_class_name) {
                 Log::debug('File match with table: ' . $name);
@@ -24,35 +26,30 @@ class Annotation {
 
     protected static function getTableStructure($table) {
         $description = '';
-        $fields = DB::select('DESC ' . $table);
+        $fields = DB::select("select column_name, data_type, character_maximum_length, is_nullable, column_default from INFORMATION_SCHEMA.COLUMNS where table_name = '$table'");
 
         foreach ($fields as $field) {
-            $type = $field->{'Type'};
+            $type = $field->{'data_type'};
             if (1==preg_match('/^int/', $type)) {
                 $type = 'int';
             } elseif ($type == 'timestamp') {
                 $type = 'int';
-            } elseif (1==preg_match('/^varchar/', $type)) {
+            } elseif (1==preg_match('/^varying/', $type)) {
                 $type = 'string';
             } else {
-                // pass
+              // pass
             }
 
-            if ($field->{'Null'} === 'YES') {
+            if ($field->{'is_nullable'} === 'YES') {
                 $type = "$type|null";
             }
 
-            $default = $field->{'Default'};
+            $default = $field->{'column_default'};
             if ('' == $default) {
                 $default = 'null';
             }
-
-            $key = $field->{'Key'};
-            if ('' == $key) {
-                $key = 'nil';
-            }
-
-            $description = $description . "@property $type \${$field->{'Field'}} Type: {$field->{'Type'}}, Key: $key\n";
+            // $description = $description . "@property $type \${$field->{'Field'}} Type: {$field->{'Type'}}, Key: $key\n";
+            $description = $description . "@field \${$field->{'column_name'}} $type \n"; # Type: {$field->{'data_type'}}\n";
         }
 
         return $description;
